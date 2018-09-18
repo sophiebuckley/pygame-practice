df8cbbcc2549a131ebd1cf1603fe254dbffdbfb5
Sophie Buckley <sophiebuckley08@live.co.uk>
2018-09-18 09:53:47 +0100

    Fixed increment.

--- ballexample.py
+++ ballexample.py
@@ -4,6 +4,7 @@ pygame.font.init()
 size = width, height = 320, 240
 speed = [2, 2]
 black = 0, 0, 0
+white = 255, 255, 255
 clock = pygame.time.Clock()
 font = pygame.font.SysFont('Arial', 40)
 score = 0
@@ -11,16 +12,19 @@ score = 0
 screen = pygame.display.set_mode(size)
 
 #Load in ball image
-ball = pygame.image.load("intro_ball.png")
+#ball = pygame.image.load("intro_ball.png")
 #Make ball a 'rect' object --> 'https://www.pygame.org/docs/ref/rect.html#pygame.Rect'
-ballrect = ball.get_rect()
+#ballrect = ball.get_rect()
 
 #While True = True i.e. when program is running
 while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
 
+    ballrect = pygame.draw.circle(screen, white, (160, 120), 20, 0)
+
     ballrect = ballrect.move(speed)
+
     #'Collision' detected by establishing whether or not the ball object is outside the bounds of the size of the game window
     if ballrect.left < 0 or ballrect.right > width:
         #Direction of speed is set to the opposite direction
@@ -38,9 +42,10 @@ while 1:
 
     screen.fill(black)
     #screen.blit --> 'https://www.pygame.org/docs/ref/surface.html#pygame.Surface.blit'
-    screen.blit(ball, ballrect)
+    #screen.blit(ball, ballrect)
     screen.blit(fps, (20, 10))
     screen.blit(scoretext, (260, 200))
     #pygame.display.flip() --> 'https://stackoverflow.com/questions/29314987/difference-between-pygame-display-update-and-pygame-display-flip'
     pygame.display.flip()
+    pygame.display.update()
     clock.tick(60)

3b3463313405b0064b4781d36ad83ebe5657692f
Sophie Buckley <sophiebuckley08@live.co.uk>
2018-09-24 22:11:38 +0100

    Changed how the ball moves and also made the colour of the ball change upon collision.

--- ballexample.py
+++ ballexample.py
@@ -1,10 +1,21 @@
-import sys, pygame
+import sys, pygame, random
 pygame.font.init()
 
-size = width, height = 320, 240
-speed = [2, 2]
+size = width, height = 480, 360
+speed = [4, 4]
 black = 0, 0, 0
 white = 255, 255, 255
+red = 255, 0, 0
+orange = 255, 128, 0
+yellow = 255, 255, 0
+dark_green = 0, 128, 0
+green = 173, 255, 47
+cyan = 0, 238, 238
+blue = 0, 0, 255 
+purple = 155, 48, 255
+hot_pink = 255, 52, 179
+pink = 255, 192, 203
+colours = [red, orange, yellow, dark_green, green, cyan, blue, purple, hot_pink, pink]
 clock = pygame.time.Clock()
 font = pygame.font.SysFont('Arial', 40)
 score = 0
@@ -14,37 +25,42 @@ screen = pygame.display.set_mode(size)
 #A transparent surface with per-pixel alpha
 ball = pygame.Surface((60, 60), pygame.SRCALPHA)
 #Draw the ball on the 'ball' surface --> 'https://stackoverflow.com/questions/44471789/python-typeerror-argument-1-must-be-pygame-surface-not-pygame-rect'
-ballrect = pygame.draw.circle(ball, [255, 255, 255], [30, 30], 20)
+ball_colour = random.choice(colours)
+ball_x = 30
+ball_y = 30
 
 #While True = True i.e. when program is running
 while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
 
-    #ballrect = pygame.draw.circle(screen, white, (160, 120), 20, 0)
-
-    ballrect = ballrect.move(speed)
+    ball_x += speed[0]
+    ball_y += speed[1]
+    screen.fill(black)
+    ball_rect = pygame.draw.circle(screen, ball_colour, [ball_x, ball_y], 20)
 
     #'Collision' detected by establishing whether or not the ball object is outside the bounds of the size of the game window
-    if ballrect.left < 0 or ballrect.right > width:
+    if ball_rect.left <= 0 or ball_rect.right >= width:
         #Direction of speed is set to the opposite direction
         speed[0] = -speed[0]
+        #Randomly set the colour of the ball
+        ball_colour = random.choice(colours)
         #'Collision' counter is incremented
         score += 1
-    if ballrect.top < 0 or ballrect.bottom > height:
+
+    if ball_rect.top <= 0 or ball_rect.bottom >= height:
         speed[1] = -speed[1]
+        ball_colour = random.choice(colours)
         score += 1
 
     #clock.getfps gives a float, which is changed to an int in order to display less figures, then a string to allow font functions to be used
     counter = str(int(clock.get_fps()))
     fps = font.render(counter, False, (255, 20, 147))
-    scoretext = font.render(str(score), False, (255, 20, 147))
+    score_text = font.render(str(score), False, (255, 20, 147))
 
-    screen.fill(black)
     #screen.blit --> 'https://www.pygame.org/docs/ref/surface.html#pygame.Surface.blit'
-    screen.blit(ball, ballrect)
     screen.blit(fps, (20, 10))
-    screen.blit(scoretext, (260, 200))
+    screen.blit(score_text, (440, 320))
     #pygame.display.flip() --> 'https://stackoverflow.com/questions/29314987/difference-between-pygame-display-update-and-pygame-display-flip'
     pygame.display.flip()
     pygame.display.update()
